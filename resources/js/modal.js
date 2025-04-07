a4e15a390aba58c663ed502d2dfaf12ee8b871fa
VictorRMarcelino <httpsvictorramos@gmail.com>
2025-04-07 02:28:32 +0000

    Refactory

---
+++ resources/js/modal.js
@@ -0,0 +1,8 @@
+var Modal = {
+
+    /**
+     * Open a modal with the specified ID
+     * @param {string} sId 
+     */
+    open: function(sId) {}
+}
---
+++ resources/js/todolist.js
@@ -0,0 +1,9 @@
+var Todolist = {
+    
+    /**
+     * Open the modal to add an item
+     */
+    openModalAddItem: function(){
+        Modal.open('modal_add_item');
+    }
+}
